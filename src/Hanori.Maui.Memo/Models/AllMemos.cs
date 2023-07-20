3eed23f4bf20baa0f01daef1392c76f7e00bfed7
leehanol <dev.hanori@gmail.com>
2023-07-21 06:43:20 +0900

    Memo _ 모델 추가

---
+++ src/Hanori.Maui.Memo/Models/AllMemos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Hanori.Maui.Memo.Models
+{
+    public class AllMemos : DbContext
+    {
+        DbSet<Memo> Memos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "";
+            var serverVersion = new MariaDbServerVersion(new Version(10, 3, 2));
+            optionsBuilder.UseMySql(connectionString, serverVersion)
+                                .LogTo(Console.WriteLine, LogLevel.Information)
+                                .EnableSensitiveDataLogging()
+                                .EnableDetailedErrors();
+        }
+    }
+}
---
+++ src/Hanori.Maui.Memo/Models/Memo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanori.Maui.Memo.Models
+{
+    public class Memo
+    {
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+}
